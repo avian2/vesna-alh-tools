671764d3fa0b8304a50cdeb805a6fa1c510f7797
Zoltan Padrah <zoltan.padrah@ijs.si>
2012-10-10 17:36:36 +0200

    update dat2csv.py to print frequencies in the table

--- examples/dat2csv.py
+++ examples/dat2csv.py
@@ -3,6 +3,7 @@ Script to convert a .dat file to a .csv file
 Usage: data2csv.ph input_dat_file output_csv_file
 
 Format of the output file:
+0         , frequency_1,      frequency_2,          , frequency_N
 timestamp1, power1_channel_1, power1_channel_2, ... , power1_channel_N
 timestamp2, power2_channel_1, power2_channel_2, ... , power2_channel_N
 ...
@@ -22,6 +23,8 @@ def main():
 
     data = []
     output = []
+    freq_table = []
+    trace_number = 0
     
     for line in open(input_filename,"r"):
         if line[0] == "#":
@@ -29,10 +32,14 @@ def main():
         parts = line.split()
         if len(parts) > 0 :
             time = parts[0]
+            freq = parts[1]
             power = parts[2]
             if len(data) == 0:
                 # in first element of a line, place a time-stamp
                 data.append(time)
+                trace_number += 1
+            if trace_number == 1:
+                freq_table.append(freq)
             data.append(power)
         else:
             # new block
@@ -41,6 +48,12 @@ def main():
 
 
     fout = open(output_filename, "w")
+    # frequency table header
+    fout.write("0")
+    for freq in freq_table:
+        fout.write(", %s" % (freq))
+    fout.write("\n")
+    # power values
     for line in output:
         fout.write(line[0])
         for value in line[1:]:
