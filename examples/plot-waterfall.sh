17700bdcb85957350c9763b61793493ab08986a8
Tomaz Solc <tomaz.solc@tablix.org>
2012-10-12 14:50:17 +0200

    Add axis labels to waterfall plot.

--- examples/plot-waterfall.sh
+++ examples/plot-waterfall.sh
@@ -3,9 +3,13 @@
 gnuplot << EOF
 
 set title '$1'
-set timefmt "%s"
+set xlabel "f [MHz]"
+set ylabel "t [s]"
+set cblabel "P [dBm]"
+set grid
+unset key
 set pm3d map corners2color c1
-splot "$1" using 2:1:3
+splot "$1" using (\$2/1e6):1:3
 pause mouse
 EOF
 #
