a47172e734cab2c23798c4441c7f5a41dadb995a
Tomaz Solc <tomaz.solc@tablix.org>
2015-07-20 09:57:56 +0200

    Set version to 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 	return open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 setup(name='vesna-alhtools',
-      version='0.1',
+      version='1.0.0',
       description='Tools for talking the VESNA almost-like-HTTP protocol',
       license='GPL',
       long_description=get_long_description(),
